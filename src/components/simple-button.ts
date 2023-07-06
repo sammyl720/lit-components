2e443a8ac16fd53120d5c71345e2d0a2e653854c
sam <sam.leider@hotmail.com>
2023-07-06 14:18:45 -0400

    cleanup

--- src/components/simple-button.ts
+++ src/components/simple-button.ts
@@ -1,14 +1,25 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('lwc-simple-button')
 export class SimpleButton extends LitElement {
 
-  static styles = css`p { color: blue; }`;
+  static styles = css`
+    .btn { 
+      outline: none;
+      border: 1px solid black;
+      border-radius: 4px;
+      box-shadow: 1px 1px 20px -14px rgba(0,0,0,0.1);
+      background: transparent;
+      padding: 3px 6px;
+    }`
+    ;
 
   @property({ type: String })
   text = 'Simple Button';
   render() {
-    return html`<p>${this.text}</p>`;
+    const classes = { btn: true }
+    return html`<button class=${classMap(classes)}>${this.text}</button>`;
   }
 }
